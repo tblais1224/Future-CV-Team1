fa995ef9d115088762732058d0d8efb8f4f26e60
tblais1224 <thomasblais.student@careerdevs.com>
2019-06-24 17:23:10 -0400

    components folder and layout files being developed

---
+++ client/src/App.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { Router, Route } from "react-router-dom"
+
+//import components
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
+import NotFound from "./components/not-found/NotFound";
+
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+
+            <Route exact path="/not-found" component={NotFound} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    )
+  }
+}
+
+export default App
---
+++ client/src/components/not-found/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      <h1 className="display-4">Page Could Not Be Found</h1>
+      <p>The page you are trying to access not exist. Sorry!</p>
+    </div>
+  );
+}
