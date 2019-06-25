a9be85c3006c08deb43685a08ad29b5adbacda3f
tblais1224 <thomasblais.student@careerdevs.com>
2019-06-25 13:57:55 -0400

    working on html resume profile

---
+++ client/src/components/profiles/MessagesResumeProfile.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MessagesResumeProfile extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
