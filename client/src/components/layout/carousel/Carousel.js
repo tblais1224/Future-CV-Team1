df3169f42bed6a7161a26896a7220ed76d5d545d
unknown <tmartinez114@gmail.com>
2019-06-26 14:39:43 -0400

    made carousel only 4 images

--- client/src/components/layout/carousel/Carousel.js
+++ client/src/components/layout/carousel/Carousel.js
@@ -19,45 +19,5 @@ export default () => (
       <img src="http://lorempixel.com/output/cats-q-c-640-480-4.jpg" alt="" />
       <p className="legend">Legend 4</p>
     </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-5.jpg" alt="" />
-      <p className="legend">Legend 5</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-6.jpg" alt="" />
-      <p className="legend">Legend 6</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-7.jpg" alt="" />
-      <p className="legend">Legend 7</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-8.jpg" alt="" />
-      <p className="legend">Legend 8</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-9.jpg" alt="" />
-      <p className="legend">Legend 9</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-10.jpg" alt="" />
-      <p className="legend">Legend 10</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-11.jpg" alt="" />
-      <p className="legend">Legend 11</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-12.jpg" alt="" />
-      <p className="legend">Legend 12</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-13.jpg" alt="" />
-      <p className="legend">Legend 13</p>
-    </div>
-    <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-14.jpg" alt="" />
-      <p className="legend">Legend 14</p>
-    </div>
   </Carousel>
 );
