d3f73d7e10e15805d7dae6b655ad1eeb08723871
unknown <tmartinez114@gmail.com>
2019-06-26 14:35:32 -0400

    Carousel is up and running from reference of codesandbox

--- client/src/components/layout/Landing.jsx
+++ client/src/components/layout/Landing.jsx
@@ -1,22 +1,14 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from "react-responsive-carousel";
+import Carousel from "./carousel/Carousel.js";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 class Landing extends Component {
   render() {
     return (
-      <Carousel>
-        <div>
-          <img
-            src="https://via.placeholder.com/150
-
-C/O https://placeholder.com/"
-            alt=""
-          />
-          <p className="legend">Legend 1</p>
-        </div>
-      </Carousel>
+      <div>
+        <Carousel />
+      </div>
     );
   }
 }
