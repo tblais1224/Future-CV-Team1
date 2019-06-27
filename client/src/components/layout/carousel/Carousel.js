860126c8fa06f5c19360152122928d175d353318
unknown <tmartinez114@gmail.com>
2019-06-26 21:58:07 -0400

    added images to carousel/images/ to swap for cat photos -- but doesn't work. Have to figure out how to make local images appear

--- client/src/components/layout/carousel/Carousel.js
+++ client/src/components/layout/carousel/Carousel.js
@@ -4,19 +4,19 @@ import { Carousel } from "react-responsive-carousel";
 export default () => (
   <Carousel autoPlay>
     <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-1.jpg" alt="" />
+      <img src="./images/365daysofcode.jpg" alt="" />
       <p className="legend">Resume Image 1</p>
     </div>
     <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-2.jpg" alt="" />
+      <img src="./images/batman_logo.jpg" alt="" />
       <p className="legend">Resume Image 2</p>
     </div>
     <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-3.jpg" alt="" />
+      <img src="./images/batman-wallpaper-1920x1080.jpg" alt="" />
       <p className="legend">Resume Image 3</p>
     </div>
     <div>
-      <img src="http://lorempixel.com/output/cats-q-c-640-480-4.jpg" alt="" />
+      <img src="./images/batman.jpg" alt="" />
       <p className="legend">Resume Image 4</p>
     </div>
   </Carousel>
