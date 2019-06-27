83690156a5bb39a277043de2f09fcb0721befe49
unknown <tmartinez114@gmail.com>
2019-06-26 21:46:51 -0400

    added new title to images in Carousel

--- client/src/components/layout/carousel/Carousel.js
+++ client/src/components/layout/carousel/Carousel.js
@@ -5,19 +5,19 @@ export default () => (
   <Carousel autoPlay>
     <div>
       <img src="http://lorempixel.com/output/cats-q-c-640-480-1.jpg" alt="" />
-      <p className="legend">Legend 1</p>
+      <p className="legend">Resume Image 1</p>
     </div>
     <div>
       <img src="http://lorempixel.com/output/cats-q-c-640-480-2.jpg" alt="" />
-      <p className="legend">Legend 2</p>
+      <p className="legend">Resume Image 2</p>
     </div>
     <div>
       <img src="http://lorempixel.com/output/cats-q-c-640-480-3.jpg" alt="" />
-      <p className="legend">Legend 3</p>
+      <p className="legend">Resume Image 3</p>
     </div>
     <div>
       <img src="http://lorempixel.com/output/cats-q-c-640-480-4.jpg" alt="" />
-      <p className="legend">Legend 4</p>
+      <p className="legend">Resume Image 4</p>
     </div>
   </Carousel>
 );
