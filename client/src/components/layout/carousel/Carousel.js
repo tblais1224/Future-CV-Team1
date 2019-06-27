812a34532062bc17e78b51346c3c3f9ee752f366
unknown <tmartinez114@gmail.com>
2019-06-26 22:00:12 -0400

    put cat images back but left the dead photo links

--- client/src/components/layout/carousel/Carousel.js
+++ client/src/components/layout/carousel/Carousel.js
@@ -4,20 +4,36 @@ import { Carousel } from "react-responsive-carousel";
 export default () => (
   <Carousel autoPlay>
     <div>
+      <img src="http://lorempixel.com/output/cats-q-c-640-480-1.jpg" alt="" />
+      <p className="legend">Legend 1</p>
+    </div>
+    <div>
+      <img src="http://lorempixel.com/output/cats-q-c-640-480-2.jpg" alt="" />
+      <p className="legend">Legend 2</p>
+    </div>
+    <div>
+      <img src="http://lorempixel.com/output/cats-q-c-640-480-3.jpg" alt="" />
+      <p className="legend">Legend 3</p>
+    </div>
+    <div>
+      <img src="http://lorempixel.com/output/cats-q-c-640-480-4.jpg" alt="" />
+      <p className="legend">Legend 4</p>
+    </div>
+    <div>
       <img src="./images/365daysofcode.jpg" alt="" />
-      <p className="legend">Resume Image 1</p>
+      <p className="legend">365 Days - Image 1</p>
     </div>
     <div>
       <img src="./images/batman_logo.jpg" alt="" />
-      <p className="legend">Resume Image 2</p>
+      <p className="legend">Batman Logo - Image 2</p>
     </div>
     <div>
       <img src="./images/batman-wallpaper-1920x1080.jpg" alt="" />
-      <p className="legend">Resume Image 3</p>
+      <p className="legend">Batman - Image 3</p>
     </div>
     <div>
       <img src="./images/batman.jpg" alt="" />
-      <p className="legend">Resume Image 4</p>
+      <p className="legend">Batman - Image 4</p>
     </div>
   </Carousel>
 );
